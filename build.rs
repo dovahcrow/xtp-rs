1941eda2fab4f36d7f40ec41ea4b4c65b18ad2cc
Weiyüen Wu <doomsplayer@gmail.com>
2020-01-26 23:45:09 -0800

    grant LLVM_CONFIG_PATH awareness to build script

--- build.rs
+++ build.rs
@@ -12,6 +12,7 @@ fn main() {
         .compile("bridge");
 
     add_search_path();
+    add_llvm_path();
 
     println!("cargo:rustc-link-lib=xtptraderapi");
     println!("cargo:rustc-link-lib=xtpquoteapi");
@@ -46,27 +47,22 @@ fn main() {
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+    bindings.write_to_file(out_path.join("bindings.rs")).expect("Couldn't write bindings!");
 }
 
 #[cfg(not(target_os = "windows"))]
 fn add_search_path() {
-    for path in std::env::var("LD_LIBRARY_PATH")
-        .unwrap_or_else(|_| "".to_string())
-        .split(":")
-    {
+    for path in std::env::var("LD_LIBRARY_PATH").unwrap_or_else(|_| "".to_string()).split(":") {
+        if path.trim().len() == 0 {
+            continue;
+        }
         println!("cargo:rustc-link-search={}", path);
     }
 }
 
 #[cfg(target_os = "windows")]
 fn add_search_path() {
-    for path in std::env::var("PATH")
-        .unwrap_or_else(|_| "".to_string())
-        .split(";")
-    {
+    for path in std::env::var("PATH").unwrap_or_else(|_| "".to_string()).split(";") {
         if path.trim().len() == 0 {
             continue;
         }
@@ -80,4 +76,10 @@ fn check_arch() {}
 #[cfg(target_arch = "x86")]
 fn check_arch() {
     unimplemented!("Not implemented for 32 bit system!");
+}
+
+fn add_llvm_path() {
+    if let Some(llvm_config_path) = option_env!("LLVM_CONFIG_PATH") {
+        println!("cargo:rustc-env=LLVM_CONFIG_PATH={}", llvm_config_path);
+    }
 }
